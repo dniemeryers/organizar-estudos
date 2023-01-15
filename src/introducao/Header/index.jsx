5f09363ce0838723a80efbb13b28cc9c1315666d
Diego Niemeyer <102764313+dniemeryers@users.noreply.github.com>
2023-01-15 13:54:03 -0300

    1

---
+++ src/introducao/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container, Wrapper,  Row} from './styles';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <Container>
+          <Row>
+        
+          </Row>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export { Header }
